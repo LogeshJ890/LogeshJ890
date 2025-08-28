0173582902ecebded7dc3c1da1def432e2c90715
LogeshJ890 <logeshj8982@gmail.com>
2025-08-28 12:24:50 +0530

    Update pointer to variable program.C

--- pointer to variable program.C
+++ pointer to variable program.C
@@ -9,9 +9,9 @@ int main() {
    s = &p;
    
    
-    printf("Value of the variable P: %d\n",p);
-    printf("Address of the variable p :%d\n",&p);
-    printf("The value stroed in the variable s :%d\n",s);
-    printf("The Address of the vaeiable s : %d\n",&s);
-    printf("The value stored in the address of the variable s :%d",*s);
+    printf("Value of the variable P: %d\n",p);  //prints 300
+    printf("Address of the variable p :%d\n",&p); // prints address of p
+    printf("The value stored in the variable s :%d\n",s);  //prints value of the s
+    printf("The Address of the vaeiable s : %d\n",&s);  // prints the address of the s
+    printf("The value stored in the address of the variable s :%d",*s); // prints the value stored in the address
 }
